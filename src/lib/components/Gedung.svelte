f4d8188d5ad9f0de7b5ab0517a78b749ee226c7a
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-11-28 13:54:26 +0700

    gedung views

---
+++ src/lib/components/Gedung.svelte
@@ -0,0 +1,31 @@
+<script>
+  import ProductTable from './ProductTable.svelte';
+  
+  let products = [
+    { vendor: '', unitPrice: '' }
+  ];
+  
+  const columns = [
+    { key: 'gedung', label: 'Gedung', type: 'input', placeholder: 'Enter gedung name' },
+    { key: 'unitPrice', label: 'Unit Price', type: 'number' }
+  ];
+  
+  function addProduct() {
+    products = [...products, { vendor: '', unitPrice: '' }];
+  }
+  
+  function removeProduct(index) {
+    products = products.filter((_, i) => i !== index);
+  }
+</script>
+
+<div>
+  <h2 class="text-2xl font-bold text-gray-800 mb-6">Gedung</h2>
+  
+  <ProductTable
+    {columns}
+    data={products}
+    onAddProduct={addProduct}
+    onRemoveProduct={removeProduct}
+  />
+</div>
